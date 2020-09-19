e4bcca2afc528838ecac769a4e9c9d4e470ee55b
Christian Wiegand <cwiegand@wgdnet.de>
2020-09-19 17:34:23 +0200

    Change to rest with json

--- src/main.rs
+++ src/main.rs
@@ -3,52 +3,56 @@
 #[macro_use]
 extern crate rocket;
 extern crate bayespam;
+extern crate serde;
 
-#[cfg(test)]
-mod tests;
-
-use bayespam::classifier;
 use bayespam::classifier::Classifier;
+use rocket_contrib::json::Json;
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+#[derive(Deserialize)]
+struct Message {
+    text: String,
+    is_spam: bool,
 }
 
-#[get("/hello/<name>")]
-fn hi(name: String) -> String {
-    format!("Hi {}", name)
-}
-
-#[get("/train/<message>/<is_spam>")]
-fn train(message: String, is_spam: bool) -> Result<(), std::io::Error> {
-    println!("Receives Message: {}", message);
-    println!("Classified as Spam?: {}", is_spam.to_string());
-
-    let mut classifier_file = File::open("model.json")?;
-    let mut classifier = Classifier::new_from_pre_trained(&mut classifier_file)?;
-
-    if is_spam {
-        classifier.train_spam(&message);
-    } else {
-        classifier.train_ham(&message);
-    }
-
-
-    classifier.save(&mut classifier_file, true)?;
-
-    Ok(())
+#[derive(Serialize)]
+struct Rating {
+    spam: bool,
+    score: f32,
 }
 
 #[get("/check/<message>")]
-fn check(message: String) -> String {
-    println!("Receives Message: {}", message);
-    classifier::identify(&message).unwrap().to_string()
+fn check(message: String) -> Json<Rating> {
+    let mut classifier_file = File::open("model.json").unwrap();
+    let classifier = Classifier::new_from_pre_trained(&mut classifier_file).unwrap();
+
+    let is_spam = classifier.identify(&message);
+    let score = classifier.score(&message);
+
+    let rat = Rating {
+        spam: is_spam,
+        score,
+    };
+
+    Json(rat)
+}
+
+#[post("/train", format = "json", data = "<msg>")]
+fn train(msg: Json<Message>) {
+    let mut classifier_file = File::open("model.json").unwrap();
+    let mut classifier = Classifier::new_from_pre_trained(&mut classifier_file).unwrap();
+
+    if msg.is_spam {
+        classifier.train_spam(&msg.text);
+    } else {
+        classifier.train_ham(&msg.text);
+    }
+
+    let mut file = File::create("model.json").unwrap();
+    classifier.save(&mut file, false).unwrap();
 }
 
 fn main() {
-    rocket::ignite()
-        .mount("/", routes![hello, hi, check, train])
-        .launch();
+    rocket::ignite().mount("/", routes![check, train]).launch();
 }
