829f1bdf23bd92edebfc46e1a8b338d5dfffe0af
Christian Wiegand <cwiegand@wgdnet.de>
2020-09-26 15:42:03 +0200

    Check api key and user agent

--- src/main.rs
+++ src/main.rs
@@ -34,27 +34,30 @@ struct Rating {
 struct ApiKey(String);
 
 /// Returns true if `key` is a valid API key string.
-fn is_valid(key: &str) -> bool {
-    key == "valid_api_key"
+fn is_valid(key: &str, agent: &str) -> bool {
+    key == "valid_api_key" && agent == "test-client"
 }
+
 
 #[derive(Debug)]
 enum ApiKeyError {
     BadCount,
-    Missing,
     Invalid,
 }
 
 impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
     type Error = ApiKeyError;
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-
         let keys: Vec<_> = request.headers().get("x-api-key").collect();
-        match keys.len() {
-            0 => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
-            1 if is_valid(keys[0]) => Outcome::Success(ApiKey(keys[0].to_string())),
-            1 => Outcome::Failure((Status::Forbidden, ApiKeyError::Invalid)),
-            _ => Outcome::Failure((Status::BadRequest, ApiKeyError::BadCount)),
+        let agents: Vec<_> = request.headers().get("User-Agent").collect();
+        if agents.len() == 1 || keys.len() == 1 {
+            if is_valid(keys[0], agents[0]) {
+                Outcome::Success(ApiKey(keys[0].to_string()))
+            } else {
+                Outcome::Failure((Status::Forbidden, ApiKeyError::Invalid))
+            }
+        } else {
+            Outcome::Failure((Status::BadRequest, ApiKeyError::BadCount))
         }
     }
 }
