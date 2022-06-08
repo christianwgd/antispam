5bb4ab3042ad1ab0e5e3cb2670bdbef9de8e2006
Christian Wiegand <christianwgd@users.noreply.github.com>
2022-06-08 21:59:26 +0200

    Use rocket 0.5

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use]
 extern crate rocket;
 extern crate bayespam;
@@ -7,12 +5,11 @@ extern crate clap;
 extern crate serde;
 
 use bayespam::classifier::Classifier;
-use clap::{crate_authors, App, Arg};
+use clap::{Arg, Command};
 use rocket::http::Status;
-use rocket::request::FromRequest;
-use rocket::Outcome;
-use rocket::{request, Request, State};
-use rocket_contrib::json::Json;
+use rocket::request::{FromRequest, Outcome};
+use rocket::serde::json::Json;
+use rocket::{Request, State};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::error::Error;
@@ -37,12 +34,7 @@ struct Rating {
     score: f32,
 }
 
-struct ApiKey(String);
-
-/// Returns true if `key` is a valid API key string.
-fn is_valid(key: &str, agent: &str, clients: &Value) -> bool {
-    clients[agent] == key
-}
+struct ApiKey<'r>(&'r str);
 
 #[derive(Debug)]
 enum ApiKeyError {
@@ -50,15 +42,22 @@ enum ApiKeyError {
     Invalid,
 }
 
-impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ApiKey<'r> {
     type Error = ApiKeyError;
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        let clients = request.guard::<State<Value>>().unwrap().inner();
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        /// Returns true if `key` is a valid API key string.
+        fn is_valid(key: &str, agent: &str, clients: &Value) -> bool {
+            clients[agent] == key
+        }
+
+        let clients = request.guard::<&State<Value>>().await.unwrap();
         let keys: Vec<_> = request.headers().get("x-api-key").collect();
         let agents: Vec<_> = request.headers().get("User-Agent").collect();
         if agents.len() == 1 || keys.len() == 1 {
-            if is_valid(keys[0], agents[0], clients) {
-                Outcome::Success(ApiKey(keys[0].to_string()))
+            if is_valid(keys[0].to_string().as_ref(), agents[0], clients) {
+                Outcome::Success(ApiKey(keys[0]))
             } else {
                 Outcome::Failure((Status::Forbidden, ApiKeyError::Invalid))
             }
@@ -69,7 +68,7 @@ impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
 }
 
 #[post("/check", format = "json", data = "<msg>")]
-fn check(_key: ApiKey, msg: Json<CheckMessage>, model_file: State<String>) -> Json<Rating> {
+fn check(_key: ApiKey<'_>, msg: Json<CheckMessage>, model_file: &State<String>) -> Json<Rating> {
     let mut classifier_file = File::open(&model_file.inner()).unwrap();
     let classifier = Classifier::new_from_pre_trained(&mut classifier_file).unwrap();
 
@@ -85,7 +84,7 @@ fn check(_key: ApiKey, msg: Json<CheckMessage>, model_file: State<String>) -> Js
 }
 
 #[post("/train", format = "json", data = "<msg>")]
-fn train(_key: ApiKey, msg: Json<Message>, model_file: State<String>) {
+fn train(_key: ApiKey, msg: Json<Message>, model_file: &State<String>) {
     let mut classifier_file = File::open(&model_file.inner()).unwrap();
     let mut classifier = Classifier::new_from_pre_trained(&mut classifier_file).unwrap();
 
@@ -99,24 +98,26 @@ fn train(_key: ApiKey, msg: Json<Message>, model_file: State<String>) {
     classifier.save(&mut file, false).unwrap();
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let matches = App::new("antispam")
+#[allow(unused_must_use)]
+#[rocket::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let matches = Command::new("antispam")
         .version("1.0")
-        .author(crate_authors!())
+        .author("Christian Wiegand <cwiegand@wgdnet.de>")
         .about("Antispam is a web service runtime for the bayespam spam checker crate.")
         .arg(Arg::new("config")
             .required(true)
             .short('c')
             .long("config")
             .value_name("CONFIG_FILE")
-            .about("Sets a custom config file in json format containing agents and their api-keys as key-value pairs.")
+            .long_help("Sets a custom config file in json format containing agents and their api-keys as key-value pairs.")
             .takes_value(true))
         .arg(Arg::new("model")
             .required(true)
             .short('m')
             .long("model")
             .value_name("MODEL_FILE")
-            .about("Sets the model file for the bayes values. Model file is created if it doesn't exist.")
+            .long_help("Sets the model file for the bayes values. Model file is created if it doesn't exist.")
             .takes_value(true))
         .get_matches();
 
@@ -132,11 +133,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         classifier.save(&mut classifier_file, false)?;
     }
 
-    rocket::ignite()
+    rocket::build()
         .mount("/", routes![check, train])
         .manage(clients)
         .manage(model)
-        .launch();
-
+        .launch()
+        .await?;
     Ok(())
 }
